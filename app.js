d1e6c4fc7d113a04cff69e2da64107c568a00616
CSRuggeri <santiruu@gmail.com>
2023-11-15 20:06:24 -0300

    correccion

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
@@ -17,6 +17,6 @@ app.get('/register', (req,res)=>{
 
 
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log('Servidor funcionando');
 });
